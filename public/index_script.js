4fc226f31b8011ee7cabf7e5ac159c2b533f08d0
jscudMainDevice <jscudweiss@gmail.com>
2022-03-27 23:46:44 -0400

    new commit

--- public/index_script.js
+++ public/index_script.js
@@ -1,68 +1,65 @@
-
-
 function loadCar(car) {
     return `    
     <div class="row car_row">
-                <div class="col-4 imgDiv"">
-                    <img src=${car.url} alt="...">
+                <div class="col-5 imgDiv">
+                    <img src="${car.url}" alt="...">
                 </div>
-                <div class="col-8 text-div" style="margin-top: 2rem">
+                <div class="col-7 text-div" style="margin-top: 2rem">
                     <div class="row">
-                        <div class="col-10">
+                        <div class="col-11">
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-3">
                                     <p class="text">Stock Number</p>
                                 </div>
-                                <div class="col-6">
+                                <div class="col-9">
                                     <p class="make text">${car.stock_num}</p>
                                 </div>
                                 <hr>
                             </div>
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-3">
                                     <p class="text">Make</p>
                                 </div>
-                                <div class="col-6">
+                                <div class="col-9">
                                     <p class="make text">${car.make}</p>
                                 </div>
                                 <hr>
                             </div>
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-3">
                                     <p class="text">Model</p>
                                 </div>
-                                <div class="col-6">
+                                <div class="col-9">
                                     <p class="model text">${car.model}</p>
                                 </div>
                                 <hr>
                             </div>
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-3">
                                     <p class="text">Year</p>
                                 </div>
-                                <div class="col-6">
+                                <div class="col-9">
                                     <p class="year text">${car.year}</p>
                                 </div>
                                 <hr>
                             </div>
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-3">
                                     <p class="text">Price</p>
                                 </div>
-                                <div class="col-6">
+                                <div class="col-9">
                                     <p class="price text">$${car.price}</p>
                                 </div>
                                 <hr>
                             </div>
+                            <div class="row">
+                                <button type="button" class="btn btn-danger btn-block delete_btn">Delete</button>
+                            </div>
                         </div>
-                        <div class="col-2">
-                            <input type="checkbox" class="carCheck"/>
+                        <div class="col-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
                         </div>
                     </div>
-                    <div class="row">
-                        <button type="button" class="btn btn-danger btn-block delete_btn" value='${JSON.stringify(car)}'>delete</button>
-                    </div>
-
                 </div>
             </div>
     `
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + "/public"));
 app.listen(3000, function () {
     console.log("server started at 3000");
     const rawData = fs.readFileSync(__dirname + "/public/data/data10.json");
-    carList=JSON.parse(rawData);
+    carList = JSON.parse(rawData);
     console.log(carList);
 });
 
@@ -28,47 +28,50 @@ app.get("/", function (req, res) {
     console.log()
 });
 
-app.get("/new-car", function (req, res) {
-    res.sendFile("/public/new_car.html");
-});
+app.post('/delete-car', (req, res) => {
+    carList = carList.filter((car) => {
+        //console.log((car.url !== req.body.url) , (car.color !== req.body.color) , (car.stock_num !== req.body.stock_num) , (car.make !== req.body.make) , (car.model !== req.body.model), (parseInt(car.year) !== parseInt(req.body.year)), (parseInt(car.price) !== parseInt(req.body.price)))
+        return ((car.url !== req.body.url) || (car.color !== req.body.color) || (car.stock_num !== req.body.stock_num) || (car.make !== req.body.make) || (car.model !== req.body.model) || (parseInt(car.year) !== parseInt(req.body.year)) || (parseInt(car.price) !== parseInt(req.body.price)));
+    });
+    //console.log(carList);
+    const carJSON = JSON.stringify(carList);
+    fs.writeFile(__dirname + "/public/data/data10.json", carJSON,
+        function (err) {
+            if (err) {
+                console.log("Json writing failed", err);
+            } else {
+                res.redirect('/');
+            }
+        });
+})
 
-app.post("/create-new-car", (req,res)=> {
+app.post("/get-new-car", (req, res) => {
     //console.log(req.body.car);
     const carItem = {
         "stock_num": req.body.stock_num,
         "make": req.body.make,
         "model": req.body.model,
         "year": parseInt(req.body.year),
-        "url":req.body.url,
-        "color":req.body.color,
+        "url": req.body.url,
+        "color": req.body.color,
+        "price": parseInt(req.body.price),
     }
     carList.push(carItem);
     const carJSON = JSON.stringify(carList);
-    console.log(carJSON);
-    fs.writeFile(__dirname+"/public/data/data10.json", carJSON, function (err) {
-        if(err){
+    //console.log(carJSON);
+    fs.writeFile(__dirname + "/public/data/data10.json", carJSON, function (err) {
+        if (err) {
             console.log("Json writing failed", err);
-        }else {
+        } else {
             res.redirect('/');
         }
     });
     //res.send("Thank You!");
 })
 
-app.get('/create-new-car',(req,res)=>{
-    res.sendFile(__dirname+"/public/index.html");
-})
-app.post('/delete-car',(req,res)=>{
-    carList = carList.filter((car)=>{
-        return ((car.url !== req.body.url) || (car.color !== req.body.color) || (car.stock_num !== req.body.stock_num) || (car.make !== req.body.make) || (car.model !== req.body.model) || (car.year !== req.body.year) || (car.price !== req.body.price));
-    });
-    const carJSON = JSON.stringify(carList);
-    fs.writeFile(__dirname+"/public/data/data10.json", carJSON,
-        function (err) {
-            if(err){
-                console.log("Json writing failed", err);
-            }else {
-                res.redirect('/');
-            }
-        });
-})
+app.get("/new-car", function (req, res) {
+    res.sendFile(__dirname + "/public/new_car.html");
+});
+app.post("/new-car", function (req, res) {
+    res.sendFile(__dirname + "/public/new_car.html");
+});
